d981ec051d8d45dc4a17ddfc6e4bfc8bc2f5f0e6
Russ Paul-Jones <russ@paul-jones.org>
2020-02-18 08:46:13 -0800

    Display solving sequence in place in Windows terminal window

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,3 +1,6 @@
+from os import system, name
+from time import sleep
+
 simpleSudoku = [
 	[1, 2, 3, 4, 5, 6, 7, 8, 9],
 	[4, 5, 6, 7, 8, 9, 1, 2, 3],
@@ -10,7 +13,7 @@ simpleSudoku = [
 	[9, 1, 2, 3, 4, 5, 6, 7, 8],
 	]
 
-board = [
+hardBoard = [
 	[0,0,0,4,0,6,0,0,0],
 	[7,0,0,2,0,0,0,9,0],
 	[0,0,0,0,5,3,0,0,4],
@@ -22,6 +25,20 @@ board = [
 	[0,2,0,0,0,0,7,0,0]
 	]
 
+easyBoard = [
+	[0,0,0,0,0,4,6,7,0],
+	[0,0,9,2,0,0,8,0,1],
+	[0,0,7,6,1,3,0,4,9],
+	[0,5,0,1,0,0,2,8,4],
+	[0,1,0,0,0,0,3,9,6],
+	[4,9,6,8,0,0,0,5,0],
+	[3,0,0,0,6,1,0,2,0],
+	[0,8,5,4,0,0,0,6,0],
+	[9,0,0,0,7,8,0,0,0],
+	]
+
+board = easyBoard
+
 def PrintRow(row):
 	global board
 	for col in range(9):
@@ -29,11 +46,16 @@ def PrintRow(row):
 			print('.', end='')
 		else:
 			print(board[row][col], end='')
+		if col == 5 or col == 2:
+			print('|', end='')
 	print()
 
 def PrintBoard():
+	system("cls")
 	for row in range(9):
 		PrintRow(row)
+		if row==2 or row==5:
+			print("---+---+---")
 
 def CanPlaceInRow(n, row):
 	global board
@@ -70,10 +92,12 @@ def Solve():
 				for trial in range(1, 10):
 					if CanPlace(trial, row, col):
 						board[row][col] = trial
+						PrintBoard()
 						Solve()
 				board[row][col] = 0
 				return
 	PrintBoard()
+	sleep(4)
 
 PrintBoard()
 print("Solving...")
