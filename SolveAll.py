f2037708b8901a11b5edeb67b7c095f863b8e06f
Russ Paul-Jones <russ@paul-jones.org>
2021-08-02 23:22:17 -0700

    Implement SolveAll for the whole puzzle set
    Outputs a key value for Problem 96 in Project Euler: https://projecteuler.net/problem=96

--- SolveAll.py
+++ SolveAll.py
@@ -1,2 +1,26 @@
 # SolveAll.py
 # solve all of the puzzles and report the solutions
+
+from Puzzle import ParsePuzzleStrings
+from SudokuSolver import SudokuSolver
+
+def Main():
+	puzzles = ParsePuzzleStrings()
+	keySum = 0
+	for puzzle in puzzles:
+		board = puzzle[1]
+		trials = 0
+		solver = SudokuSolver(board, yieldLevel=0)
+		generator = solver.Generate()
+		for result in generator:
+			# result = next(generator)
+			trials += 1
+			if result == 2:
+				line = solver.board[0]
+				key = line[0]*100 + line[1]*10 + line[2]
+				keySum += key
+				print("After {} trials, we found a solution for {} with a key: {}".format(trials, puzzle[0], key))
+	print("The final sum of keys is {}".format(keySum))	
+
+if __name__ == '__main__':
+	Main()
