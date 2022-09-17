256244cfdaacd992d11448ed9169b3915c926c6f
Russ Paul-Jones <russ@paul-jones.org>
2022-09-16 23:44:02 -0700

    User solver count of trials

--- SolveAll.py
+++ SolveAll.py
@@ -32,17 +32,15 @@ def Main(arguments):
 	startTime = time()
 	for puzzle in puzzles:
 		board = puzzle[1]
-		trials = 0
 		solver = SudokuSolver(board, fast, yieldLevel=0)
 		generator = solver.Generate()
 		for result in generator:
 			# result = next(generator)
-			trials += 1
 			if result == 2:
 				line = solver.board[0]
 				key = line[0]*100 + line[1]*10 + line[2]
 				keySum += key
-				print(f"After {trials} trials, we found a solution for {puzzle[0]} with a key: {key}")
+				print(f"After {solver.positionsTried} trials, we found a solution for {puzzle[0]} with a key: {key}")
 	endTime = time()
 	print(f"After {endTime-startTime} seconds, the final sum of keys is {keySum}")	
 
